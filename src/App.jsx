97ca8590a3edc1bb09906a7f88b65c7272d52384
Pedro Fiel <pedro.fiel@shakersagencia.com.br>
2024-08-26 10:58:17 -0300

    feat: Criando componente de cabecalho e iniciando no styledComponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components"
+import EstilosGlobais from "./components/EstilosGlobais";
+import Cabecalho from "./components/Cabecalho";
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
- 
-    </>
+    <FundoGradiente>
+      <EstilosGlobais />
+      <Cabecalho />
+    </FundoGradiente>
   )
 }
 
