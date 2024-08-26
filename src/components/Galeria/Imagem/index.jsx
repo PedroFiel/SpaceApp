f2b66356afc1004f5f3a4cd2b3d82480518d0eaa
Pedro Fiel <pedro.fiel@shakersagencia.com.br>
2024-08-26 15:19:45 -0300

    feat: Estilização modal

--- src/components/Galeria/Imagem/index.jsx
+++ src/components/Galeria/Imagem/index.jsx
@@ -2,7 +2,7 @@ import { styled } from "styled-components"
 import BotaoIcone from "../../BotaoIcone"
 
 const Figure = styled.figure`
-    width: ${props => props.$expandida ? '90%' : '460px'};
+    width: ${props => props.$expandida ? '75%' : '460px'};
     max-width: 100%;
     margin: 0;
     display: flex;
